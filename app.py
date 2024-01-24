70cb288408561498bf0589616ed96fc6820845c4
TVRZru <120438824+TVRZru@users.noreply.github.com>
2024-01-24 17:08:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'data.db'  # Замените путь к файлу
+db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    product = db.Column(db.String(50))
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nickname = request.form['nickname']
+        password = request.form['password']
+        product = request.form['product']
+
+        user = User(nickname=nickname, password=password, product=product)
+        db.session.add(user)
+        db.session.commit()
+
+        return 'Данные успешно сохранены!'
     return render_template('index.html')
-
-@app.route('/register', methods=['POST'])
-def register():
-    username = request.form['username']
-    password = request.form['password']
-    # Здесь можно добавить код для сохранения данных пользователя
-
-    return render_template('index.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
