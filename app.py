5e6e23533d237628e880aebc602c047ab3b1cbb0
TVRZru <120438824+TVRZru@users.noreply.github.com>
2024-01-24 08:41:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Список пользователей (обычно хранится в базе данных)
-users = []
-
-
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
 
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
     password = request.form['password']
+    # Здесь можно добавить код для сохранения данных пользователя
 
-    # Создаем нового пользователя и добавляем его в список
-    user = {
-        'username': username,
-        'password': password
-    }
-    users.append(user)
-
-    return redirect(url_for('success', username=username))
-
-
-@app.route('/success/<username>')
-def success(username):
-    return f'Registration successful! Welcome, {username}!'
-
+    return render_template('success.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
